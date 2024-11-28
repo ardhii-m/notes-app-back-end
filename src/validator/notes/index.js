e19ce8b6d2ab1906744790e87b194290491ab106
ardhii-m <ardhiim008@gmail.com>
2024-11-28 11:28:02 +0700

    postgres database implemented

--- src/validator/notes/index.js
+++ src/validator/notes/index.js
@@ -1,7 +1,6 @@
 const InvariantError = require('../../exceptions/InvariantError');
 const { NotePayloadSchema } = require('./schema');
 
-
 const NotesValidator = {
   validateNotePayload: (payload) => {
     const validationResult = NotePayloadSchema.validate(payload);
